fb1bd3b6c556c1f932b7a23ba3d0359bf6402737
Sudhanshu Iyer <sudhanshu.iyer@tangomc.com>
2015-04-16 13:56:15 -0500

    Converted to Maven Project. Simplified FileSystem and re-wrote FileSystemTests Throwing errors up the stack for the client to handle.

--- src/main/java/edu/utdallas/aos/p3/FileSystem.java
+++ src/main/java/edu/utdallas/aos/p3/FileSystem.java
@@ -1,6 +1,9 @@
 package edu.utdallas.aos.p3;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -10,114 +13,71 @@ import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class FileSystem {
-	
+
 	static final Logger logger = LogManager.getLogger(FileSystem.class);
-	File []listOfFiles=null;
-	String rootFolderPath="";
-	//gets the OS based delimiter
-	String fileDelimiter=System.getProperty("file.separator");
-	FileSystem(String root)
-	{
-		File rootFolder=new File(root);
-		listOfFiles=rootFolder.listFiles();
-		this.rootFolderPath=root;
-		try
-		{
-			if(listOfFiles==null)
-				throw new NullPointerException();
-			if(listOfFiles.length==0)
-				throw new Exception();
+	File[] listOfFiles = null;
+	String rootFolderPath = "";
+
+	// gets the OS based delimiter
+	String fileDelimiter = System.getProperty("file.separator");
+
+	FileSystem(String root) {
+		File rootFolder = new File(root);
+		boolean rootFolderExists = rootFolder.exists();
+
+		if (rootFolderExists) {
+			listOfFiles = rootFolder.listFiles();
+			this.rootFolderPath = root;
+		} else {
+			logger.error("The directory: " + rootFolder + " does not exist ");
 		}
-		catch(NullPointerException n)
-		{
-			logger.error("The directory: "+rootFolder+" does not exist ");
-			//System.out.println("The directory: "+rootFolder+" does not exist ");
+		if (listOfFiles.length < 1) {
+			logger.error("There are no files in the directory " + rootFolder);
 		}
-		catch(Exception e)
-		{
-			logger.error("There are no files in the directory "+rootFolder);
-			//System.out.println("There are no files in the directory "+rootFolder);
-		}
-		
+
 	}
-	public String read(String fileName) 
-	{
-		//rootFolderPath has the root information
-		String path=rootFolderPath+fileDelimiter+fileName;
-		Scanner scan = null;
-		String line ="";
-		try {
-			scan = new Scanner(new File(path));
-			line=scan.nextLine();
-			
-		} catch (FileNotFoundException e) {
-			logger.error("The absolute file path: "+path+" was not found");
-			//System.out.println("The absolute file path: "+path+" was not found");
-			//e.printStackTrace();
-		}
-		catch(NoSuchElementException e)
-		{
-			logger.error("No line found in the file");
-			//System.out.println("No line found in the file");
-		}
-		finally{
-			scan.close();
-		}
+
+	public String read(String fileName) throws FileNotFoundException, NoSuchElementException {
+		// rootFolderPath has the root information
+		String path = rootFolderPath + fileDelimiter + fileName;
+		Scanner scanner = null;
+		String line = "";
+		scanner = new Scanner(new File(path));
+		line = scanner.nextLine();
+		scanner.close();
 		return line;
-		
+
 	}
-	
-	public void write(String fileName,String data)
-	{
-		//path of the file
-		String filepath=rootFolderPath+fileDelimiter+fileName;
-		FileWriter f=null;
-		try {
-			//Check whether File Exists
-			if(new File(filepath).exists())
-			{
-				f=new FileWriter(filepath);
-				//check data standard
-				if(data==null)
-					try {
-						throw new Exception();
-					} catch (Exception e) {
-						logger.error("Check the data while Writing to a File: "+filepath);
-						//System.out.println("Check the data while Writing to a File: "+filepath);
-						
-					}
-				//write to data only if we are done with all check
-				f.write(data);
-				
-			}
-			else
-				throw new FileNotFoundException();
+
+	public void write(String fileName, String data) throws IOException {
+		// path of the file
+		String filepath = rootFolderPath + fileDelimiter + fileName;
+		File file = new File(filepath);
 		
+		if(!file.exists()){
+			logger.error("File Not found.");
+			throw new FileNotFoundException("File at " + filepath + " not found.");
+		}
 		
+		if(data == null){
+			data = "";
+			logger.warn("Writing empty line to file.");
 		}
-		catch (FileNotFoundException e) 
-		{
-			logger.error("File does not exist for writing at absolute path:"+filepath);
-			//System.out.println("File does not exist for writing at absolute path:"+filepath);
-		}
-		catch (IOException e) {
 		
-			e.printStackTrace();
-		}
-		finally{
-			try {
-				f.close();
-			} catch (IOException e) {
-				logger.error("File close did not happen properly in finally()");
-				//System.out.println("File close did not happen properly in finally()");
-				//e.printStackTrace();
-			}
+		BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+		
+		try{
+			writer.write(data);
+		} catch(IOException execption) {
+			throw execption;
+		} finally {
+			writer.close();
 		}
 	}
-	public ArrayList<String> getListOfFile()
-	{
-		ArrayList<String> list=new ArrayList<String>();
-		for(File f: listOfFiles)
+
+	public ArrayList<String> getListOfFile() {
+		ArrayList<String> list = new ArrayList<String>();
+		for (File f : listOfFiles)
 			list.add(f.getAbsolutePath());
 		return list;
 	}
--- src/test/java/edu/utdallas/aos/p3/FileSystemTest.java
+++ src/test/java/edu/utdallas/aos/p3/FileSystemTest.java
@@ -2,7 +2,10 @@ package edu.utdallas.aos.p3;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 import org.junit.Test;
 
@@ -10,40 +13,50 @@ import org.junit.Test;
  * Unit test for simple App.
  */
 public class FileSystemTest {
-	
+
 	/*
 	 * A Test Case
 	 */
 	@Test
-	public void testSomething(){
-		FileSystem fs=new FileSystem("root0");
-		String output=fs.read("1.txt");
+	public void testRead() {
+		FileSystem fs = new FileSystem("root0");
+		String output = null;
+		try {
+			output = fs.read("1.txt");
+		} catch (FileNotFoundException | NoSuchElementException e) {
+
+			e.printStackTrace();
+		}
 		System.out.println(output);
 		assertEquals(output, "Hello");
-		/*
-		 * Create some mock objects
-		 * Check if one or two methods work correctly 
-		 * use assertEquals();
-		 */
 	}
 
 	@Test
-	public void testSomethingElse(){
-		/*
-		 * Create some mock objects
-		 * Check some other methods work correctly 
-		 * use assertEquals();
-		 */
-		FileSystem fs=new FileSystem("root0");
-		fs.write("2.txt","abc");
+	public void testWrite() {
+
+		FileSystem fs = new FileSystem("root0");
+		String readLine = null;
+		try {
+			fs.write("2.txt", "abc");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			readLine = fs.read("2.txt");
+		} catch (FileNotFoundException | NoSuchElementException e) {
+			e.printStackTrace();
+		}
+		readLine = readLine.trim();
+		System.out.println(readLine);
+		assertEquals("abc", readLine);
 	}
+
 	@Test
-	public void testListOfFiles()
-	{
-		FileSystem f=new FileSystem("root1");
-		ArrayList<String> output=f.getListOfFile();
-		for(String s:output)
+	public void testListOfFiles() {
+		FileSystem f = new FileSystem("root0");
+		ArrayList<String> output = f.getListOfFile();
+		for (String s : output)
 			System.out.println(s);
-		
+
 	}
 }
